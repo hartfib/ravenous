b8290ca18c2c45aff7c0a44a7026d2458bdc1489
Ivy Hartfield <hartfib@lycos.com>
2017-11-21 17:54:55 -0700

    v1 Ravenous completed

---
+++ src/util/Yelp.js
@@ -0,0 +1,48 @@
+const clientId = 'BqU3sJI6EnVvvS0O-0bI6w';
+const secret = 'gVoRxVbz3XiBnI8c1zVfuHu6d5D7AMrdQNYBi1EpIkDe1aSULQIfHGQHttqR8TOj';
+let accessToken ='';
+
+let Yelp = {
+
+	getAccessToken(){
+		if(accessToken){
+			return new Promise(resolve => resolve(accessToken));
+		}
+		return fetch(`https://cors-anywhere.herokuapp.com/https://api.yelp.com/oauth2/token?grant_type=client_credentials&client_id=${clientId}&client_secret=${secret}`, {
+			method: 'POST'}).then(response => {
+  			return response.json();
+		});
+	}, // end getAccessToken
+
+	search(term, location, sortBy){
+		return Yelp.getAccessToken().then( () => {
+			return fetch(`https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?term=${term}&location=${location}&sort_by=${sortBy}`, {
+				headers: {Authorization: `Bearer ${accessToken}`, 'origin' : 'origin'}
+			}) // end fetch
+		}).then(response => {
+  			return response.json();
+			}).then(jsonResponse => {
+				if(jsonResponse.businesses){
+					return jsonResponse.businesses.map(business => {
+						return{
+							id: business.id,
+  							imageSrc: business.image_url,
+  							name: business.name,
+  							address: business.location.address1,
+  							city: business.location.city,
+  							state: business.location.state,
+  							zipCode: business.location.zip_code,
+  							category: business.categories[0].title,
+  							rating: business.rating,
+  							reviewCount: business.review_count
+						}
+						
+					})//close map
+				}// close if
+			});
+
+	} // end search method
+
+}; //end yelp object
+
+export default Yelp;
